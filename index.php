b5fdbc76c7f6bea8c0554ad7f286a23d0e1c2710
frank <archipfe@gmail.com>
2018-08-25 21:42:36 +0900

    sku | product name (english) | product name (japanese) | short description

--- index.php
+++ index.php
@@ -3,6 +3,34 @@
 
 $url = 'innisfree-products.json';
 $json = file_get_contents($url);
+$json = str_replace("[[[", "[", $json);
+
 $data = json_decode($json);
+
+echo "
+
+   
+";
+
+
+foreach ($data as $key => $value) {
+	echo "<ul style='overflow: '>";
+	echo "<li >".$value->sku."</li>";
+	echo "<li >".$value->product_name_en."</li>";
+	echo "<li>".$value->product_name_ja."</li>";
+	echo "<li >".$value->short_description_ja."</li>";
+	echo "</ul>";
+
+}
+
+
+echo "
+    
+
+";
+
+echo $json;
+
+
 
 ?>
