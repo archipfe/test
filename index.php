9c0886a46d7e25364a4aca85e67cb266e2ef836a
frank <archipfe@gmail.com>
2018-08-25 18:55:19 +0900

    get contents

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 $url = 'innisfree-products.json';
-
+$json = file_get_contents($url);
 
 ?>
--- index2.php
+++ index2.php
@@ -1,6 +1,6 @@
 <?php
 
 $url = 'innisfree-products.json';
-
+$get_data = file_get_contents($url);
 
 ?>
